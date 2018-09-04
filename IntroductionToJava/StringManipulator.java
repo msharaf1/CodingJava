a1572ae4f745486c046203f7fa4a76802039dad2
msharaf1 <menhaj0102@gmail.com>
2018-09-04 17:49:07 -0400

    commit

---
+++ IntroductionToJava/StringManipulator.java
@@ -0,0 +1,12 @@
+public class StringManipulator {
+    public static void main(String[] args){
+     String h = "    Hello    ";
+     String w = "  World   ";
+
+    //  String hw = h.concat(w);
+     System.out.println(h.trim() + " " +w.trim());
+
+     
+     
+    }
+}
