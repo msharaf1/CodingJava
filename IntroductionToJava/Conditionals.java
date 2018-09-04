42e51356ed5baffe1fd19d1c04e15632bd50b4f9
msharaf1 <menhaj0102@gmail.com>
2018-09-04 13:45:22 -0400

    commit

---
+++ IntroductionToJava/Conditionals.java
@@ -0,0 +1,27 @@
+public class Conditionals {
+    public static void main(String[] args) {
+        boolean defenderGuarding = true;
+        boolean defenderGuarding1 = false;
+        if(defenderGuarding == true) {
+            System.out.println("Pass The Ball!");
+        }
+
+        if(defenderGuarding1 ==true) {
+            System.out.println("Def is set to False");
+        } 
+        else {
+            System.out.println("Shoot the ball");
+        }
+
+    }
+}
+/* 
+boolean defenderGuarding = true;
+boolean closeToBasket = true;
+if(defenderGuarding == true) {
+    System.out.println("Pass The Ball");
+} else if(closeToBasket == true) {
+    System.out.println("Shoot The Ball");
+} else {
+    System.out.println("Dribble The Ball");
+} */
