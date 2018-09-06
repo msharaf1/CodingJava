1f510fffed139c45bfaa6b15d9106985538dcbc5
msharaf1 <menhaj0102@gmail.com>
2018-09-05 20:26:26 -0400

    commit

---
+++ IntroductionToJava/weekOne/testAndDemoClass/javaOOP/ArrayA.java
@@ -0,0 +1,13 @@
+public class ArrayA{
+    
+    int[] myArray;
+
+
+    ArrayA(){
+        myArray = new int[2];
+        myArray[0] = 10;
+        myArray[1] = 20;
+        
+    }
+
+            }
---
+++ IntroductionToJava/weekOne/testAndDemoClass/javaOOP/ArrayATest.java
@@ -0,0 +1,7 @@
+public class ArrayATest{
+    public static void main(String[] args){
+        ArrayA myArrayB = new ArrayA();
+        System.out.println(myArrayB.myArray[0]);
+        System.out.println(myArrayB.myArray[1]);
+    }
+}
