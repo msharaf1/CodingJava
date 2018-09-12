230bdb38dd46a027cb17ca73f242edb60fdf8619
msharaf1 <menhaj0102@gmail.com>
2018-09-12 08:16:39 -0400

    commit

--- IntroductionToJava/weekOne/singleFilesClass/JavaBasics.java
+++ IntroductionToJava/weekOne/singleFilesClass/JavaBasics.java
@@ -13,26 +13,26 @@ public class JavaBasics {
 		// int[] myArrays = new int[] {1, 2, 3, 4};
 
 		// basics.printArray();
-		int[] myArrays = new int[] {-2, -5, 20, 19, 200, -1, 500, 9000};
-		basics.findMax(myArrays);
-
+		int[] myArrays = new int[] {2, 4, 8, 10};
+		// basics.findMax(myArrays);
+		basics.getAvg(myArrays);
 
 
   }
 
-    public  void print1to255() {
-        for (int i = 1; i <= 255; i++) {
-            System.out.println(i);
-        }
-    }
+    // public  void print1to255() {
+    //     for (int i = 1; i <= 255; i++) {
+    //         System.out.println(i);
+    //     }
+    // }
 
-    public void printOdd1To255(){
-    	for(int i = 0; i<=255; i++){
-    		if(i % 2 != 0){
-    		System.out.println(i);
-    			}
-    		}
-    }
+    // public void printOdd1To255(){
+    // 	for(int i = 0; i<=255; i++){
+    // 		if(i % 2 != 0){
+    // 		System.out.println(i);
+    // 			}
+    // 		}
+    // }
 
     // public void printSum1To255(){
     // 	int count=0;
@@ -44,36 +44,48 @@ public class JavaBasics {
     // 	}
 
     // }
-    public void printSum1To255(){
-    	int sum = 0;
-    	for(int i = 0; i <=255; i++){
-    		sum += i;
-    	System.out.println(sum);
-    	}
+    // public void printSum1To255(){
+    // 	int sum = 0;
+    // 	for(int i = 0; i <=255; i++){
+    // 		sum += i;
+    // 	System.out.println(sum);
+    // 	}
 
-    }
-
-    public void printArray(){
-    	int[] myArrays = new int[] {1,3,5,7,9,13};
-    		for(int i = 0; i < myArrays.length; i++){
-    			System.out.println(myArrays[i]);
-    		}
-    }
-    public void findMax(int[] myArrays){
-    	
-    	int max = myArrays[0];
-    	// int maxi = myArrays[i];
-    	for(int i = 0; i < myArrays.length; i++){
-    		if(myArrays[i] > max){
-    			max = myArrays[i];
-    		}
-    	}
-    	System.out.println(max);
-    }
-
-    // public void printMax(){
-    // 	int[] myArrays = new int[];
     // }
+
+    // public void printArray(){
+    // 	int[] myArrays = new int[] {1,3,5,7,9,13};
+    // 		for(int i = 0; i < myArrays.length; i++){
+    // 			System.out.println(myArrays[i]);
+    // 		}
+    // }
+    // public void findMax(int[] myArrays){
+    	
+    // 	int max = myArrays[0];
+    // 	// int maxi = myArrays[i];
+    // 	for(int i = 0; i < myArrays.length; i++){
+    // 		if(myArrays[i] > max){
+    // 			max = myArrays[i];
+    // 		}
+    // 	}
+    // 	System.out.println(max);
+    // }
+
+    public void getAvg(int[] myArrays){
+    	int sum = 0;
+    	
+    	
+    	for(int i = 0; i < myArrays.length; i++){
+    		System.out.println("Value of i is: " +i);
+    		System.out.println("Sum before addition: " + sum);
+    		sum += myArrays[i];
+    		System.out.println("Sum After addition: " + sum);
+    	}
+    	float avg = sum / myArrays.length;
+    	// System.out.println(sum);
+    	System.out.println("Average is : " + avg);
+    	// System.out.println(avg);
+    }
 
 
 }
