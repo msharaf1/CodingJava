d414d21ebc8ea8593e3f373731f8d037f67f96c9
msharaf1 <menhaj0102@gmail.com>
2018-12-18 16:28:33 -0500

    commit

---
+++ IntroductionToJava/PT/basicsPt/HelloWorld.java
@@ -0,0 +1,27 @@
+// public class HelloWorld {
+//     public static void main(String[] arg){
+//         String hW = "Hello World, my Dear friend!";
+
+//         System.out.println(hW);
+//     }
+// }
+
+
+// public class HelloWorld {
+//     public static void main(String[] arg){
+//         String hW = "Hello World from VScode.";
+//         System.out.println(hW);
+//         // return hW;
+//     }
+// }
+
+public class HelloWorld{
+    public static void main(String[] arg){
+        String name = "John";
+        int DoB = 100;
+        String city = "Burbank";
+        String state = "CA";
+
+        System.out.println("My name is " + name +" and I am "+DoB +" years old." + "I live in " +city +", " + state);
+    }
+}
