1f1fccd2eac82658f1203a83f1178832097af2fc
msharaf1 <menhaj0102@gmail.com>
2018-12-21 19:28:13 -0500

    committ

---
+++ JavaBasics/OneTo255.java
@@ -0,0 +1,17 @@
+public class OneTo255{
+    private String hW = "Hello World";
+    private int OneToFv = 255;
+
+
+    
+    
+    public OneTo255(){};//constructor
+
+    public int getAllDigits(){
+        for(int i =0; i<= OneToFv; i++){
+            System.out.println(i);
+        }
+        return this.OneToFv;
+    }
+
+}
---
+++ JavaBasics/OneTo255Test.java
@@ -0,0 +1,9 @@
+public class OneTo255Test{
+    public static void main(String[] args) {
+        // System.out.println("hello Test");
+        OneTo255 allDigits = new OneTo255();
+
+        // System.out.println(allDigits.getAllDigits());
+        allDigits.getAllDigits();
+    }
+}
