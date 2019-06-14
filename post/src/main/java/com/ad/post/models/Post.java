8c5b96ce0ff43690a1c0784e14758c5b10a88838
Menhaj <menhaj@Menhajs-MacBook-Pro.local>
2019-06-13 21:06:49 -0400

    commit

--- post/src/main/java/com/ad/post/models/Post.java
+++ post/src/main/java/com/ad/post/models/Post.java
@@ -1,11 +1,18 @@
 package com.ad.post.models;
 
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 public class Post {
@@ -16,6 +23,22 @@ public class Post {
     @Size(min=1, max = 55, message="It can not be blank!")
     private String postName;
 
+    @DateTimeFormat(pattern = "yyyy/MM/dd")
+    private Date updatedAt;
+    
+    @Column(updatable = false)
+    @DateTimeFormat(pattern = "yyyy/MM/dd")
+    private Date createdAt;
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = new Date();
+    }
+    @PreUpdate
+    protected void onUpdate(){
+        this.updatedAt = new Date();
+    }
+    
     public Long getId() {
         return id;
     }
