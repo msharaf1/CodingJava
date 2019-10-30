edc5ce4e6ea827d69856b59c265496a5fadd09ff
msharaf <menhaj0102@gmail.com>
2019-10-30 06:47:18 -0400

    commit

--- codingdojojava/ninja/src/main/java/com/ninjagold/ninja/controllers/NinjaController.java
+++ codingdojojava/ninja/src/main/java/com/ninjagold/ninja/controllers/NinjaController.java
@@ -1,5 +1,7 @@
 package com.ninjagold.ninja.controllers;
 
+import java.util.Random;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
@@ -10,10 +12,23 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class NinjaController {
     int earnedGold;
+    String res;
+    Random random = new Random();
     
+    // public int getRandomNumber(int min, int max){
+    //     return random.nextInt((max-min)+1)+min;
+        
+    // }
+    // public int getRandomNumberInts(int min, int max){
+    //     return random.ints(min,(max+1)).findFirst().getAsInt();
+    // }
+
     @GetMapping("/")
     public String mainPage(HttpSession session){
-        session.getAttribute("gold");
+        if(session.getAttribute("earnedGold") == null){
+            session.setAttribute("earnedGold", 0);
+            session.setAttribute("res", "");
+        }
         session.setMaxInactiveInterval(2000);
         return "index";
     }
@@ -21,22 +36,44 @@ public class NinjaController {
     public String addFarm(HttpSession session, @RequestParam(value = "gold", required = false) String gold){
         if(gold.equals("farm")){
             if(session.getAttribute("earnedGold") ==null){
-                session.setAttribute("earnedGold", 20);
+                session.setAttribute("earnedGold", random.nextInt(10)+10);
+                res  += " house and earned "+gold+" gold.";
+                session.setAttribute("res", (String) session.getAttribute("res")+res+"\n");
                 System.out.println(session.getCreationTime());
             }
             else{
-                session.setAttribute("earnedGold", (Integer) session.getAttribute("earnedGold") + 20);
+                session.setAttribute("earnedGold", (Integer) session.getAttribute("earnedGold") + random.nextInt(10)+10);
             }
         }
         if(gold.equals("cave")){
                 if(session.getAttribute("earnedGold") ==null){
-                    session.setAttribute("earnedGold", 30);
+                    session.setAttribute("earnedGold", random.nextInt(5)+5);
                 }
                 else{
-                    session.setAttribute("earnedGold", (Integer) session.getAttribute("earnedGold") + 30);
+                    session.setAttribute("earnedGold", (Integer) session.getAttribute("earnedGold") + random.nextInt(5)+5);
                         }
                     
                 }
+        
+        if(gold.equals("house")){
+            if(session.getAttribute("earnedGold") ==null){
+                session.setAttribute("earnedGold", random.nextInt(3)+2);
+            }
+            else{
+                session.setAttribute("earnedGold", (Integer) session.getAttribute("earnedGold") + random.nextInt(3)+2);
+                    }
+                
+            }
+
+        if(gold.equals("casino")){
+            if(session.getAttribute("earnedGold") ==null){
+                session.setAttribute("earnedGold", random.nextInt(100)-50);
+            }
+            else{
+                session.setAttribute("earnedGold", (Integer) session.getAttribute("earnedGold") + random.nextInt(100)-50);
+                    }
+                
+            }
         return "redirect:/";
     }
 
--- codingdojojava/ninja/src/main/webapp/WEB-INF/index.jsp
+++ codingdojojava/ninja/src/main/webapp/WEB-INF/index.jsp
@@ -6,6 +6,11 @@
 <head>
 <meta charset="ISO-8859-1">
 <title>Ninja Gold</title>
+<style>
+    .mainContainer {
+        display: inline-block;
+    }
+</style>
 </head>
 <body>
     <div clas="mainContainer">
@@ -18,24 +23,32 @@
                 
             </div>
                 <form action="/" method="post">
+                <h1>Farm </h1>
+                <p>(ears 10-20)</p>
                     <input type="hidden" name="gold" value="farm">
                     <button name="farmButton">Find Gold!</button>
                 </form>
             </div>
             <div id="cave">
             <form action="/" method="post">
+                <h1>Cave </h1>
+                <p>(ears 5-10)</p>
                 <input type="hidden" name="gold" value="cave">
                 <button name="cave">Find Gold!</button>
                 </form>
             </div>
             <div id="house">
               <form action="/" method="post">
+              <h1>House </h1>
+                <p>(ears 2-5)</p>
                 <input type="hidden" name="gold" value="house">
                 <button name="house">Find Gold!</button>
                 </form>
             </div>
             <div id="casino">
             <form action="/" method="post">
+                <h1>Casino </h1>
+                <p>(earns/takes 0-50)</p>
                 <input type="hidden" name="gold" value="casino"> 
                 <button name="casino">Find Gold!</button>
             </form>
@@ -46,6 +59,10 @@
         </div>
 
         <div clas="activity">
+            <h1>Activities</h1>
+            <textarea id="activities">
+            <c:out value="${res}" />
+            </textarea>
         </div>
     </div>
 
